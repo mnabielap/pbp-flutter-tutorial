bd3bf12814fee5cfb66b74488a307f2f6a931be5
Muhammad Nabiel Andityo <mnabielap@gmail.com>
2022-11-17 08:30:11 +0700

    Finished Lab-8

--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial_pbp/main.dart';
+import 'package:tutorial_pbp/page/to_do_page.dart';
 
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
@@ -60,6 +61,16 @@ class _MyFormPageState extends State<MyFormPage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
